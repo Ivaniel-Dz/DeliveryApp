467ace9e73e5b6f3f486a7ed47ad4ef6dfd119f4
Ivaniel-Dz <pcivaniel@gmail.com>
2025-05-08 02:22:57 -0500

    create componente login

--- Frontend/src/app/auth/login/login.page.ts
+++ Frontend/src/app/auth/login/login.page.ts
@@ -1,15 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { IonButton, IonContent, IonIcon, IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonItem, IonLabel, IonInput, IonIcon, IonButton, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
   private fb = inject(FormBuilder);
@@ -17,6 +17,10 @@ export class LoginPage implements OnInit {
   // private auth
 
   ngOnInit() {
+  }
+
+  goToRegister(){
+    this.router.navigate(['/register']);
   }
 
 }
