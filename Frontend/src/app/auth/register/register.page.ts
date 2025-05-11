85b3ccc69235051185852c287e19891698e66a69
Ivaniel-Dz <pcivaniel@gmail.com>
2025-05-11 17:07:28 -0500

    creación del interfaces register

--- Frontend/src/app/auth/register/register.page.ts
+++ Frontend/src/app/auth/register/register.page.ts
@@ -1,8 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonButton, IonContent, IonIcon, IonInput, IonItem, IonInputPasswordToggle, IonImg } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonContent,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonInputPasswordToggle,
+  IonImg,
+} from '@ionic/angular/standalone';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -10,22 +23,43 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [ IonImg, IonInput, IonItem, IonIcon, IonContent, IonButton, IonInputPasswordToggle, CommonModule, FormsModule, ]
+  imports: [
+    IonImg,
+    IonInput,
+    IonItem,
+    IonIcon,
+    IonContent,
+    IonButton,
+    IonInputPasswordToggle,
+    CommonModule,
+    FormsModule,
+  ],
 })
-
 export class RegisterPage {
   // Inyección de dependencias
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private authService = inject(AuthService);
 
-    // Formulario
-    public form: FormGroup = this.fb.group({
-      nombre: ["", [Validators.required]],
-      email: ["", [Validators.required, Validators.email]],
-      telefono: ["",[Validators.required, Validators.pattern("^[0-9+]+$")]],
-      password: ["", [Validators.required, Validators.minLength(5)]]
-    });
+  // Formulario
+  public form: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    telefono: ['', [Validators.required, Validators.pattern('^[0-9+]+$')]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      // Aquí llamaría a su método de inicio de sesión de servicio de autores
+      // Para fines de prueba, solo navegaremos a las pestañas
+      this.router.navigate(['/login']);
+    }
+  }
+
+  registerWithGoogle() {
+    // this.authService.loginWithGoogle();
+  }
 
   goToLogin() {
     this.router.navigate(['/login']);
