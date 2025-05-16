695095413e3c6dc1e78ffa0242829c43840358b3
Ivaniel-Dz <pcivaniel@gmail.com>
2025-05-16 03:01:02 -0500

    creación y config de foodCategory component

--- Frontend/src/app/pages/food/food-category/food-category.page.ts
+++ Frontend/src/app/pages/food/food-category/food-category.page.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { Category } from '../../../interfaces/category';
+import { FoodService } from '../../../services/food.service';
+import { HeaderComponent } from '../../../components/header/header.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-food-category',
   templateUrl: './food-category.page.html',
   styleUrls: ['./food-category.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, IonicModule, HeaderComponent],
 })
 export class FoodCategoryPage implements OnInit {
+  // Inyección de Dependencias
+  private foodService = inject(FoodService);
+  private router = inject(Router);
+  // Variables
+  categories: WritableSignal<Category[]> = signal([]);
+  title = 'Categorías';
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loadCategories();
   }
 
+  // Cargar todas las categorías
+  loadCategories() {
+    this.foodService.getCategories().then((res) => this.categories.set(res));
+  }
+
+  // Ir a las comidas por categoría
+  goToFoodCategory(categoryId: number) {
+    this.router.navigate(['/tabs/food', categoryId]);
+  }
+
+  // Método para carga los imágenes alternativas
+  onImageError(event: Event) {
+    (event.target as HTMLImageElement).src = '/assets/placeholder/foods.webp';
+  }
+  
 }
