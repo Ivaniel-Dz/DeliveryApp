55cdaf677ca09999a4bf9b445bdeb5a326b5b485
Ivaniel-Dz <pcivaniel@gmail.com>
2025-05-26 00:11:53 -0500

    configuración de diseño para adaptar dispositivos

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent implements OnInit {
   constructor(private themeService: ThemeService) {}
 
   async ngOnInit() {
+    // DarkMode
     await this.themeService.loadUserPreference();
+
+    // MediaQuery
+    const isDesktop = window.innerWidth > 768 && !('ontouchstart' in window);
+    if (isDesktop) {
+      alert(
+        'Esta aplicación está diseñada para uso móvil. Para la mejor experiencia, abre esta página desde tu teléfono. Y para cambiar el modo oscuro o blanco en Configuración.'
+      );
+    }
+    
   }
 }
