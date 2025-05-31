e897318310a7758ea83e3ef34e45ba8fb6e4565e
Ivaniel-Dz <pcivaniel@gmail.com>
2025-05-30 23:48:48 -0500

    Config of OAuth Google

--- Backend/Delivery.API/Program.cs
+++ Backend/Delivery.API/Program.cs
@@ -1,14 +1,18 @@
 using Delivery.API.Data;
+using Delivery.API.Services.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Swagger: Tool to generate API automatic documentation
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 // Configuración de la base de datos (MySQL)
 var connectionString = builder.Configuration.GetConnectionString("CadenaSQL");
@@ -16,6 +20,36 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
+
+// Registrar servicios personalizados
+builder.Services.AddScoped<PasswordHashService>();
+builder.Services.AddScoped<JwtGenerator>();
+builder.Services.AddScoped<JwtValidator>();
+
+
+// Configuración de la autenticación con JWT
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Esquema predeterminado de autenticación
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Esquema predeterminado para desafíos de autenticación
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false; // No requiere HTTPS (útil en desarrollo)
+    config.SaveToken = true; // Permitir que el servidor guarde el token
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true, // Validar la clave de firma del emisor
+        ValidateIssuer = false, // No validar el emisor (puedes cambiarlo según sea necesario)
+        ValidateAudience = false, // No validar la audiencia (puedes cambiarlo según sea necesario)
+        ValidateLifetime = true, // Validar que el token no esté expirado
+        ClockSkew = TimeSpan.Zero, // No permitir margen de tiempo adicional para expiración
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]!)) // Clave de firma del token
+    };
+});
+
+
+// Crea la aplicación
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
